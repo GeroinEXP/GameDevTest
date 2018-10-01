2c5f9e2791fdf2499a92725ec24731c08382d612
DESKTOP-5CA7U2T\vsemp <vsemploxo@gmail.com>
2018-10-01 21:28:18 +0300

    Движение есть, но тпшится рандомно(надо будет разобраться). Боже, дай мне сил.

--- MMOXyNYAAAA/Assets/movementPlayer.cs
+++ MMOXyNYAAAA/Assets/movementPlayer.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class movementPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float speed = 6.0f;
+    Vector3 position1;
+
+    void Start ()
+    {
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                position1 = hit.point;
+            }
+            Vector3 direction = position1 - transform.position;
+            float targ_pos = Vector3.Distance(transform.position, position1);
+
+            if (targ_pos > 1)
+            {
+                transform.Translate(direction * speed, Space.World);
+            }
+        }
+    }
 }
